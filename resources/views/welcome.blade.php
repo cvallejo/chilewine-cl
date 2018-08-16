a2af6045b7539b748755143774eec7fe5bc391dc
Claudio Vallejo <claudio.vallejo@gmail.com>
2018-08-16 14:31:30 -0300

    arreglo nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    chilewine.cl
+                    ChileWine
                 </div>
 
                 <div class="links">
